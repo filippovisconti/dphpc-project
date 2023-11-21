4013cdae67086e5ec0fa492ba33e95981c30728e
Filippo Visconti <filippo.visconti23@gmail.com>
2023-11-21 21:20:58 +0100

    Fix formatting and edit Makefile

--- auth/benchmark.c
+++ auth/benchmark.c
@@ -2,112 +2,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "myblake.h"
 #include "reference_impl.h"
 #include "sha256.h"
-#ifdef __x86_64__
+
+#ifdef __USEPAPI__
 #include <papi.h>
 #endif
 
 #ifdef _PAPI
-#define PAPI_REGION_BEGIN(name)                                                \
-    do {                                                                       \
-        int retval = PAPI_hl_region_begin(name);                               \
-        if (retval != PAPI_OK) handle_error(1);                                \
-    } while (0)
+#define PAPI_REGION_BEGIN(name)                                                                    \
+  do {                                                                                             \
+    int retval = PAPI_hl_region_begin(name);                                                       \
+    if (retval != PAPI_OK) handle_error(1);                                                        \
+  } while (0)
 
-#define PAPI_REGION_END(name)                                                  \
-    do {                                                                       \
-        int retval = PAPI_hl_region_end(name);                                 \
-        if (retval != PAPI_OK) handle_error(1);                                \
-    } while (0)
+#define PAPI_REGION_END(name)                                                                      \
+  do {                                                                                             \
+    int retval = PAPI_hl_region_end(name);                                                         \
+    if (retval != PAPI_OK) handle_error(1);                                                        \
+  } while (0)
 
 void handle_error(int retval) {
-    printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
-    exit(1);
+  printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
+  exit(1);
 }
 #else
-#define PAPI_REGION_BEGIN(name)                                                \
-    // printf("PAPI not enabled - region %s start\n", name);
+#define PAPI_REGION_BEGIN(name)  // printf("PAPI not enabled - region %s start\n", name);
 
-#define PAPI_REGION_END(name)                                                  \
-    // printf("PAPI not enabled - region %s end\n", name);
+#define PAPI_REGION_END(name)    // printf("PAPI not enabled - region %s end\n", name);
 
 void handle_error() {
-    exit(1);
+  exit(1);
 }
 #endif
 
 void run_benchmark(char *filename) {
 #ifdef _PAPI
-    int retval;
-    (void)retval;
+  int retval;
+  (void)retval;
 #endif
 
-    // FILE *fileForPrinting;
-    FILE *file = NULL;
+  // FILE *fileForPrinting;
+  FILE *file = fopen(filename, "rb");
 
-    // Open a file, specifying which file using command line arguments
-    file = fopen(filename, "rb");
+  if (file == NULL) {
+    printf("\n[ERROR:] Could not open file %s\n", filename);
+    exit(1);
+  }
 
-    // First check to make sure the file could be found
-    if (file == NULL) {
-        printf("\n[ERROR:] Could not open file %s\n", filename);
-        exit(1);
-    }
+  printf("[INFO:] File ok, executing functions.");
 
-    // Function calls
-    printf("[INFO:] File ok, executing functions.");
+  PAPI_REGION_BEGIN("sha256");
+  calculate_sha256(file);
+  PAPI_REGION_END("sha256");
 
-    // fileForPrinting = fopen(fileName, "r");
-    // printFileContents(fileForPrinting);
+  file = fopen(filename, "r");
 
-    PAPI_REGION_BEGIN("sha256");
-    calculate_sha256(file);
-    PAPI_REGION_END("sha256");
+  if (file == NULL) {
+    printf("\n[ERROR:] Could not open file %s\n", filename);
+    exit(1);
+  }
 
-    file = fopen(filename, "r");
+  uint8_t     key[BLAKE3_KEY_LEN] = {0};
+  bool        has_key             = false;
+  const char *derive_key_context  = NULL;
+  size_t      output_len          = BLAKE3_OUT_LEN;
+  uint8_t    *output              = malloc(output_len);
 
-    if (file == NULL) {
-        printf("\n[ERROR:] Could not open file %s\n", filename);
-        exit(1);
-    }
-
-    uint8_t     key[BLAKE3_KEY_LEN] = {0};
-    bool        has_key             = false;
-    const char *derive_key_context  = NULL;
-    size_t      output_len          = BLAKE3_OUT_LEN;
-    uint8_t    *output              = malloc(output_len);
-
-    // printf("[INFO:] Starting BLAKE3 algorithm.. \n");
-    printf(
-        "=================== BLAKE3 HASH OUTPUT "
-        "==================================\n");
-    PAPI_REGION_BEGIN("blake3");
-    blake3(has_key, key, derive_key_context, output_len, file, output);
-    PAPI_REGION_END("blake3");
-    printf(
-        "======================================================================"
-        "==="
-        "\n\n");
-    free(output);
-    fclose(file);
+  // printf("[INFO:] Starting BLAKE3 algorithm.. \n");
+  printf("=================== BLAKE3 HASH OUTPUT ==================================\n");
+  PAPI_REGION_BEGIN("blake3");
+  blake3(has_key, key, derive_key_context, output_len, file, output);
+  PAPI_REGION_END("blake3");
+  printf("============================================================================\n\n");
+  free(output);
+  fclose(file);
 }
 
 int main(void) {
-    endianCheckPrint();
-    char prefix[] = "input_data/input_";
-    char suffix[] = ".txt";
+  endianCheckPrint();
+  char prefix[] = "input_data/input_";
+  char suffix[] = ".txt";
 
-    char *sizes[] = {
-        "1KB", "10KB", "100KB", "1MB", "10MB", "100MB"};  //, "1GB"};
-    size_t num_sizes = 6;
+  char  *sizes[]   = {"8KB", "32KB", "128KB", "1MB", "16MB", "128MB"};  //, "1GB"};
+  size_t num_sizes = 6;
 
-    for (size_t size = 0; size < num_sizes; size++) {
-        char filename[100];
-        sprintf(filename, "%s%s%s", prefix, sizes[size], suffix);
-        printf("[INFO:] Running benchmark for %s\n", filename);
-        run_benchmark(filename);
-    }
+  for (size_t size = 0; size < num_sizes; size++) {
+    char filename[100];
+    sprintf(filename, "%s%s%s", prefix, sizes[size], suffix);
+    printf("[INFO:] Running benchmark for %s\n", filename);
+    run_benchmark(filename);
+  }
 
-    return 0;
+  return 0;
 }
--- auth/gen_test_data.sh
+++ auth/gen_test_data.sh
@@ -1,10 +1,24 @@
 echo "Generating test data..."
 mkdir -p input_data
-base64 -i /dev/urandom | head -c 1000 >input_data/input_1KB.txt
-base64 -i /dev/urandom | head -c 10000 >input_data/input_10KB.txt
-base64 -i /dev/urandom | head -c 100000 >input_data/input_100KB.txt
-base64 -i /dev/urandom | head -c 1000000 >input_data/input_1MB.txt
-base64 -i /dev/urandom | head -c 10000000 >input_data/input_10MB.txt
-base64 -i /dev/urandom | head -c 100000000 >input_data/input_100MB.txt
-base64 -i /dev/urandom | head -c 1000000000 >input_data/input_1GB.txt
+# base64 -i /dev/urandom | head -c 1000 >input_data/input_1KB.txt
+# base64 -i /dev/urandom | head -c 10000 >input_data/input_10KB.txt
+# base64 -i /dev/urandom | head -c 100000 >input_data/input_100KB.txt
+# base64 -i /dev/urandom | head -c 1000000 >input_data/input_1MB.txt
+# base64 -i /dev/urandom | head -c 10000000 >input_data/input_10MB.txt
+# base64 -i /dev/urandom | head -c 100000000 >input_data/input_100MB.txt
+# base64 -i /dev/urandom | dd bs=1K count=8 > input_data/input_8KB.txt
+# base64 -i /dev/urandom | dd bs=1K count=32 > input_data/input_32KB.txt
+# base64 -i /dev/urandom | dd bs=1K count=128 > input_data/input_128KB.txt
+# base64 -i /dev/urandom | dd bs=1M count=1 > input_data/input_1MB.txt
+# base64 -i /dev/urandom | dd bs=1K count=16000 > input_data/input_16MB.txt
+# base64 -i /dev/urandom | dd bs=1M count=128 > input_data/input_128MB.txt
+# base64 -i /dev/urandom | head -c 1000000000 >input_data/input_1GB.txt
+dd if=/dev/urandom of=input_data/input_8KB.txt bs=1K count=8
+dd if=/dev/urandom of=input_data/input_32KB.txt bs=1K count=32
+dd if=/dev/urandom of=input_data/input_128KB.txt bs=1K count=128
+dd if=/dev/urandom of=input_data/input_1MB.txt bs=1M count=1
+dd if=/dev/urandom of=input_data/input_16MB.txt bs=1M count=16
+dd if=/dev/urandom of=input_data/input_128MB.txt bs=1M count=128
+dd if=/dev/urandom of=input_data/input_1GB.txt bs=1G count=1
+
 echo "Done."
