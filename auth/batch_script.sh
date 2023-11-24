313f60be7202581fde91c827c032be5ea49ee678
Filippo Visconti <filippo.visconti23@gmail.com>
2023-11-24 17:02:26 +0100

    Edit bash scripts

--- auth/batch_script.sh
+++ auth/batch_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -n 30
+#SBATCH -n 32
 #SBATCH --mem-per-cpu=2000
 #SBATCH --job-name=blake3_sha256
-#SBATCH --output=a.out
-#SBATCH --error=a.err
+#SBATCH --output=blake.out
+#SBATCH --error=blake.err
+
 
 module load gcc/9.3.0 papi/7.0.1
-sh gen_test_data.sh
-make run >/cluster/home/fvisconti/make_stout.txt
+make run_mp
--- auth/gen_test_data.sh
+++ auth/gen_test_data.sh
@@ -1,25 +1,20 @@
 echo "Generating test data..."
 mkdir -p input_data
-# base64 -i /dev/urandom | head -c 1000 >input_data/input_1KB.txt
-# base64 -i /dev/urandom | head -c 10000 >input_data/input_10KB.txt
-# base64 -i /dev/urandom | head -c 100000 >input_data/input_100KB.txt
-# base64 -i /dev/urandom | head -c 1000000 >input_data/input_1MB.txt
-# base64 -i /dev/urandom | head -c 10000000 >input_data/input_10MB.txt
-# base64 -i /dev/urandom | head -c 100000000 >input_data/input_100MB.txt
-# base64 -i /dev/urandom | dd bs=1K count=8 > input_data/input_8KB.txt
-# base64 -i /dev/urandom | dd bs=1K count=32 > input_data/input_32KB.txt
-# base64 -i /dev/urandom | dd bs=1K count=128 > input_data/input_128KB.txt
-# base64 -i /dev/urandom | dd bs=1M count=1 > input_data/input_1MB.txt
-# base64 -i /dev/urandom | dd bs=1K count=16000 > input_data/input_16MB.txt
-# base64 -i /dev/urandom | dd bs=1M count=128 > input_data/input_128MB.txt
-# base64 -i /dev/urandom | head -c 1000000000 >input_data/input_1GB.txt
 dd if=/dev/urandom of=input_data/input_8KB.txt bs=1K count=8
 dd if=/dev/urandom of=input_data/input_32KB.txt bs=1K count=32
 dd if=/dev/urandom of=input_data/input_64KB.txt bs=1K count=64
 dd if=/dev/urandom of=input_data/input_128KB.txt bs=1K count=128
+dd if=/dev/urandom of=input_data/input_512KB.txt bs=1K count=512
 dd if=/dev/urandom of=input_data/input_1MB.txt bs=1M count=1
+dd if=/dev/urandom of=input_data/input_2MB.txt bs=1M count=2
+dd if=/dev/urandom of=input_data/input_4MB.txt bs=1M count=4
 dd if=/dev/urandom of=input_data/input_16MB.txt bs=1M count=16
+dd if=/dev/urandom of=input_data/input_32MB.txt bs=1M count=32
+dd if=/dev/urandom of=input_data/input_64MB.txt bs=1M count=64
 dd if=/dev/urandom of=input_data/input_128MB.txt bs=1M count=128
-dd if=/dev/urandom of=input_data/input_1GB.txt bs=1G count=1
-
+dd if=/dev/urandom of=input_data/input_256MB.txt bs=1M count=256
+dd if=/dev/urandom of=input_data/input_512MB.txt bs=1M count=512
+dd if=/dev/urandom of=input_data/input_1GB.txt bs=1M count=1024
+dd if=/dev/urandom of=input_data/input_4GB.txt bs=1M count=4096
+dd if=/dev/urandom of=input_data/input_8GB.txt bs=1M count=8192
 echo "Done."
